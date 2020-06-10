4f71b064e8cd7abead6f5ef9fc3ce43c36a0910e
Berin <berin2@pdx.edu>
2020-06-10 09:07:27 -0700

    Updates

---
+++ precache-manifest.555d499ffced59d4527a0c94ec2f0c89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba91c2bac161fe413ddfb5b6a095c476",
+    "url": "/fred_dashboard/index.html"
+  },
+  {
+    "revision": "d3dfab42c5be20bd4d8e",
+    "url": "/fred_dashboard/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d5a20b29670570e71501",
+    "url": "/fred_dashboard/static/css/main.80b06fcb.chunk.css"
+  },
+  {
+    "revision": "d3dfab42c5be20bd4d8e",
+    "url": "/fred_dashboard/static/js/2.bd1d1e52.chunk.js"
+  },
+  {
+    "revision": "02ca8e3d6029d85b3c2d939e8f8dd3a6",
+    "url": "/fred_dashboard/static/js/2.bd1d1e52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5a20b29670570e71501",
+    "url": "/fred_dashboard/static/js/main.6ae8d0f6.chunk.js"
+  },
+  {
+    "revision": "fcbb6347b29d8bdec55b",
+    "url": "/fred_dashboard/static/js/runtime-main.b2c9cfc8.js"
+  }
+]);
